f835c96b42c6b0710c90285290232fbfb9e47f93
Krishna Rowter <krishna.rowter@gmail.com>
2022-09-05 15:43:17 +0700

    fix: header flickering on spam clicks

--- components/HeaderHumberger/Header/Buttons/hamburger-close.js
+++ components/HeaderHumberger/Header/Buttons/hamburger-close.js
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
-import { ANIMATION_DURATION_CLASS } from "..";
+import { ANIMATION_DURATION, ANIMATION_DURATION_CLASS } from "..";
 
 export function HamburgerCloseButton({
   isOpen: isOpenFromParent,
   onClick: onClickFromParent,
 }) {
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false)
   const [isOpenDelayed, setIsOpenDelayed] = useState(isOpenFromParent);
   const genericHamburgerLine = `h-[2px] w-6 my-1 rounded-full transition ease transform ${ANIMATION_DURATION_CLASS}`;
 
   useEffect(() => {
     setTimeout(() => setIsOpenDelayed(isOpenFromParent), 0)
+    setTimeout(() => setIsButtonDisabled(false), ANIMATION_DURATION)
   }, [isOpenFromParent])
 
   function handleClick() {
+    setIsButtonDisabled(true)
     onClickFromParent();
   }
 
@@ -21,6 +24,7 @@ export function HamburgerCloseButton({
       className={`flex flex-col h-12 w-12     border-[1px] border-${
         isOpenDelayed || isOpenFromParent ? "[#007aff]" : "white"
       } rounded-full justify-center items-center group`}
+      disabled={isButtonDisabled}
       onClick={handleClick}
     >
       <div
